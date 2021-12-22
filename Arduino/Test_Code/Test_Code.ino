b55940dd96a3db135c56e0394cc754c6084ddc11
Qaasim Rehan <34967205+qaasimrehan@users.noreply.github.com>
2021-12-22 00:25:19 -0500

    Added both scores
    Added both player scores
    Updated Nextion diplay

--- Arduino/Test_Code/Test_Code.ino
+++ Arduino/Test_Code/Test_Code.ino
@@ -1,24 +1,53 @@
+#include "Nextion.h"
+
+NexButton b1 = NexButton(10, 2, "b1");
+NexNumber timer = NexNumber(15, 7, "timer");
+NexNumber bluescore = NexNumber(15, 1, "bluescore");
 int scorePin = 3;
 int score = 0;
 int val = 0;
 int counter;
 int currentState;
 int previousState;
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(scorePin, INPUT_PULLUP);
-}
+//int bluescore = 0;
+int redscore = 0;
+//int timer;
+int duration = 30000;
+long startTime = millis();
 
-void loop() {
-  //put your main code here, to run repeatedly:
-  int val = digitalRead(scorePin);
+// Register a button object to the touch event list.  
+NexTouch *nex_listen_list[] = {
+  &b1,
+  &timer,
+  &bluescore,
+  NULL
+};
+
+void b1PopCallback(void *ptr) {
+  digitalWrite(13, HIGH);
+  delay(1000);
+ digitalWrite(13, LOW);
+  startTime = millis();
+
+  while ((millis() - startTime) < duration) {
+// update timer
+
+  timer.setValue((uint32_t)((millis() - startTime)/1000));
+  bluescore.setValue((uint32_t)((millis() - startTime)/1000));
+ // Serial.print("timer.val=");
+ // Serial.print((millis() - startTime)/1000);
+ // Serial.write(0xff);
+ // Serial.write(0xff);
+ // Serial.write(0xff);
+
+   
+ int val = digitalRead(scorePin);
   if (val == HIGH) {
-    //digitalWrite(scorePin, HIGH);
+    digitalWrite(13, HIGH);
     currentState = 1;
   }
   else {
-   // digitalWrite(scorePin, LOW);
+    digitalWrite(13, LOW);
     currentState = 0;
   }
   if (currentState != previousState) {
@@ -29,10 +58,44 @@ void loop() {
   }
   previousState = currentState;
   delay(20);
-  Serial.print("point.val=");
+
+  
+//bluescore.setValue((uint32_t)2);
+ /* Serial.print("bluescore.val=");
     Serial.print(counter);
     Serial.write(0xff);
     Serial.write(0xff);
     Serial.write(0xff);
+     ;}*/
+
+     
+
+// tState.setText("State: on");
+// digitalWrite(led1, HIGH);
+
+
+
+}
+}
+
+void setup() {
+  // put your setup code here, to run once:
+ // Serial.begin(9600);
+
+  // You might need to change NexConfig.h file in your ITEADLIB_Arduino_Nextion folder
+  // Set the baudrate which is for debug and communicate with Nextion screen
+  nexInit();
   
+  b1.attachPop(b1PopCallback, &b1);
+  
+  pinMode(scorePin, INPUT_PULLUP);
+}
+
+void loop() {
+    /*
+   * When a pop or push event occured every time,
+   * the corresponding component[right page id and component id] in touch event list will be asked.
+   */
+  nexLoop(nex_listen_list);
+
 }
